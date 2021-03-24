ad0371f4b0dcfed00bc0dfc67b0658cbe3e39310
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2021-03-24 16:16:14 +0530

    saddad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    console.log('Started');
+  })
+
+  const GetAllBlutoothDevise = () => {
+    if (navigator.bluetooth) {
+      console.log(
+        navigator.bluetooth.requestDevice({acceptAllDevices: true})
+        .then( data => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      );
+    }
+    else{
+      console.log('User Brouser Don\'t have Bluetooth Support.');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bluetooth Car</h1>
+      <button onClick={GetAllBlutoothDevise}>Start</button>
     </div>
   );
 }
